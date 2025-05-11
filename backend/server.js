de29ca4967908cddd362d37dadc17a9bcbb3026c
Hendry.535230151 <hendry.535230151@stu.untar.ac.id>
2025-05-11 21:41:02 +0700

    Adding backup file and fixing typo

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const db = require('./app/config/db');
 const userRoute = require('./app/routes/userRoute');
 const authRoute = require('./app/routes/authRoute');
+const aiRoute = require('./app/routes/aiRoute');
+const fileRoute = require('./app/routes/fileRoute');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use('/users', userRoute);
 app.use('/auth', authRoute);
+app.use('/ai', aiRoute);
+app.use('/files', fileRoute);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server run at http://localhost:${process.env.PORT}`);

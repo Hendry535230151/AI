652fc9e9f8f17f265dfd0c605c62d0060989c169
Hendry.535230151 <hendry.535230151@stu.untar.ac.id>
2025-05-12 10:42:26 +0700

    Creating model for category

---
+++ backend/app/models/Categories.js
@@ -0,0 +1,101 @@
+const db = require('../config/db');
+
+const categoryModel = {
+    getCategorie: () => {
+        return new Pormise((resolve, reject) => {
+            const query = 'SELECT * FROM categories';
+            db.query(query, (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                if (!result || result.length === 0) {
+                    resolve(false);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    },
+
+    getCategoryById: (id) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM categories WHERE id = ?';
+            db.query(query, [id], (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                if (!result || result.length) {
+                    resolve(false);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    },
+
+    getCategoryByName: (categoryName) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM categories WHERE id LIKE ?';
+            db.query(query, [categoryName], (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                if (!result || result.length === 0) {
+                    resolve(false);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    },
+
+    createCategory: (categoryName) => {
+        return new Promise((resolve, reject) => {
+            const query = 'INSERT INTO categories (category_name) VALUES (?)';
+            db.query(query, [categoryName], (err, result) => {
+                if (err) {
+                    reject(err);
+                }
+                if (!result || result.affectedRows === 0) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    },
+
+    updateCategory: (id, categoryName) => {
+        return new Promise((resolve, reject) => {
+            const query = 'UPDATE categories SET category_name = ? WHERE id = ?';
+            db.query(query, [categoryName, id], (err, result) => {
+                if (err) {
+                    reject(err);
+                } 
+                if (!result || result.affectedRows === 0) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    },
+
+    deleteCategory: (id) => {
+        return new Promise((resolve, reject) => {
+            const query = 'DELETE FROM categories WHERE id = ?';
+            db.query(query, [id], (err, result) => {
+                if (err) {
+                    reject(err);
+                } 
+                if (!result || result.affectedRows === 0) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    },
+};
+
+module.exports = categoryModel;

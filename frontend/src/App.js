b65f4f090e1e153a69f01915a8f8b2f92d04b07d
IkanDongo <noel.535230067@stu.untar.ac.id>
2025-05-17 12:26:49 +0700

    downloading and updating package, updating readme and making routing to chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Chat from './pages/Chat';
+
 
 const router = createBrowserRouter([
   { path: '/', element: <Login /> },
   { path: '/login', element: <Login /> },
   { path: '/register', element: <Register /> },
+  { path: '/chat', element: <Chat /> },
+  
 ]);
 
 function App() {
---
+++ frontend/src/pages/Chat.js
@@ -0,0 +1,10 @@
+import styles from '../css/Chat.module.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Chat() {
+    return <h1>Welcome to the Chat Page!</h1>;
+  }
+  
+  export default Chat;

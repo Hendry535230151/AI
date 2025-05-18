ee85376093678cccf28f4bd9d3b06f68d4dd513f
MakotoMachiYuki <joshua1412a.o.t@gmail.com>
2025-05-18 09:20:43 +0700

    Allowing the ai to be able to use sql query

--- backend/app/services/aiService.js
+++ backend/app/services/aiService.js
@@ -1,17 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+const handleQueryFile = require("../utils/handle_query/handleQueryFile");
+
 const aiService = {
-    aiChatting: async (message) => {
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
-        
-        const result = await model.generateContent(message);
-        const response = await result.response;
-        const text = response.text();
-        
-        return text;
-    },
+  aiChatting: async (message) => {
+    const handled = await handleQueryFile(message);
+    if (handled) return handled;
+
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
+    const result = await model.generateContent(message);
+    const response = await result.response;
+    const text = response.text();
+
+    return text;
+  },
 };
 
 module.exports = aiService;
